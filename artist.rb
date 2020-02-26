80bfcfbb4d26deb8bdbaef3254afbdc1b5deae9c
Stela Capsa <stela.capsa@gmail.com>
2020-02-26 19:56:39 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -16,9 +16,9 @@ end
 def change
   create_table :artists do |t|
     t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
+    t.string :genre
+    t.integer :age
+    t.string :hometown
   
   end
 end
